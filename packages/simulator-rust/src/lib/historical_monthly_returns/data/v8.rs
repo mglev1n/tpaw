796a0ef2cbb9a0c44938fdafb59ef4283ce67d91
Jacob Mathew <jacob@tpawplanner.com>
2026-07-27 22:02:59 +0000

    Updated Shiller data

--- packages/simulator-rust/src/lib/historical_monthly_returns/data/v8.rs
+++ packages/simulator-rust/src/lib/historical_monthly_returns/data/v8.rs
@@ -3,6 +3,5 @@ pub mod v8_empirical_stats_by_block_size_bonds;
 pub mod v8_empirical_stats_by_block_size_stocks;
 pub mod v8_raw_monthly_non_log_series;
 
-// TODO:
-// Saturday, July 25, 2026 at 11:00:00 PM UTC-07:00 DST
-pub const V8_HISTORICAL_MONTHLY_RETURNS_EFFECTIVE_TIMESTAMP_MS: i64 = 1784764800000;
+// Monday, July 27, 2026 at 3:30:00 PM UTC-07:00 DST
+pub const V8_HISTORICAL_MONTHLY_RETURNS_EFFECTIVE_TIMESTAMP_MS: i64 = 1785191400000;
